1a3649cdb3ac1dc69f5512e04962117c426556f8
Thibault <thib-17@orange.fr>
2017-09-25 20:57:47 +0200

    Test request sending

--- spec/ServiceSpec.php
+++ spec/ServiceSpec.php
@@ -6,18 +6,40 @@ use TZK\Taiga\Services\Users;
 use TZK\Taiga\Taiga;
 
 describe('ServiceSpec', function () {
+    given('url', function () {
+        return 'localhost';
+    });
+
+    given('request', function () {
+        return new CurlRequest();
+    });
+
     given('client', function () {
-        return new Taiga(new CurlRequest(), 'localhost');
+        return new Taiga($this->request, $this->url);
     });
 
     given('service', function () {
         return $service = new Users($this->client);
     });
 
-    it('sends request with valid verb', function () {
-        $verbs = Service::$ALLOWED_HTTP_VERBS;
-        foreach ($verbs as $verb) {
-            $this->service->$verb();
-        }
+    it('uses the right parameters to send the request', function () {
+        allow($this->client)
+            ->toReceive('request')
+            ->andReturn([]);
+
+        $verb = 'GET';
+        $endpoint = 'me';
+        $params = [
+            'param1' => 'value1',
+            'param2' => 'value2',
+        ];
+        $data = [];
+
+        expect($this->client)
+            ->toReceive('request')
+            ->with('GET', 'users/'.$endpoint.'?'.http_build_query($params), $data)
+            ->once();
+
+        $this->service->get($endpoint, $params, $data);
     });
 });
